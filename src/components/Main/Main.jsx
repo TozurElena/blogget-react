fe48466ebc47fa06b68281b4a28a3703c7cf2273
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-12-07 14:26:37 +0100

    modfier component Auth

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import Layout from '../Layout';
+import style from './Main.module.css';
+
+export const Main = () => {
+  return (
+    <div className={style.main}>
+      <Layout/>
+    </div>
+    
+  )
+}
