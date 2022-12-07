e31d0a0998002e6e26073560585689676812b6a4
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-12-07 13:51:12 +0100

    add components Logo and Search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
 import style from './Header.module.css';
+import Layout from '../Layout';
+import Logo from '../Header/Logo';
+import Heading from '../Header/Heading';
+import Search from '../Header/Search';
+import Auth from '../Header/Auth';
 
 export const Header = () => {
-  return <div></div>
+  return (
+    <header>
+      <Layout>
+        <Logo />
+        <Heading/>
+        <Search/>
+        <Auth/>
+      </Layout>
+    </header>
+  ) 
 }
---
+++ src/components/Header/Heading/Heading.jsx
@@ -0,0 +1,5 @@
+import style from './Heading.module.css';
+
+export const Heading = () => {
+  return <div></div>
+}
